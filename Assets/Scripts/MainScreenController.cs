300a167a6d06159e499305447e1055c1aa43b89c
HelpMeGame <keagan.a.bowman@gmail.com>
2022-11-12 19:53:01 -0500

    Main menu UI
    small bug with ports not closing after application closes

---
+++ Assets/Scripts/MainScreenController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Mirror;
+
+public class MainScreenController : MonoBehaviour
+{
+    NetworkManager networkManager;
+    
+    public TMP_InputField joinIP, joinPort, hostPort;
+    public TextMeshProUGUI joinPortError, hostPortError;
+
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+    }
+
+    public void UpdateJoinInformation()
+    {
+        networkManager.networkAddress = joinIP.text;
+
+        ushort port;
+        if (ushort.TryParse(joinPort.text, out port))
+        {
+            networkManager.GetComponent<kcp2k.KcpTransport>().Port = port;
+        }
+        else
+        {
+            joinPortError.enabled = true;
+            return;
+        }
+    }
+
+    public void UpdateHostPort()
+    {
+        ushort port;
+        if (ushort.TryParse(joinPort.text, out port))
+        {
+            networkManager.GetComponent<kcp2k.KcpTransport>().Port = port;
+        }
+        else
+        {
+            hostPortError.enabled = true;
+            return;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();   
+    }
+}
